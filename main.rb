996366ff993ca10269526c044f44503fa8dc473e
Bradley Showen <41598254+BShowen@users.noreply.github.com>
2019-02-24 12:01:39 -0500

    Refactor code.

--- main.rb
+++ main.rb
@@ -1,14 +1,10 @@
 def substring(string, dictionary)
-  contains = {}
+  contains = Hash.new(0)
   string  = string.split(/ /)
   string.each do |word|
     dictionary.each do |index|
       if word.downcase.include? index
-        if contains[index] == nil
-          contains[index] = 1
-        else
           contains[index] += 1
-        end
       end
     end
   end
